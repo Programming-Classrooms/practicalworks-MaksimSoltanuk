2d7f37b3aa45fdedf7d754af4ba68c893037d6d9
MaksimSoltanuk <soltanukmaksim@gmail.com~>
2023-12-10 14:22:38 +0300

    But you're killing my love, and it will destroy me But you're killing my love, my heart's disappointed I'll love you forever, but please don't say never I'll keep you remembered so deep in my heart

--- laboratory-task-2/main.cpp
+++ laboratory-task-2/main.cpp
@@ -9,95 +9,99 @@
 #include <cmath>
 
 
-int32_t countAmstrongNumber(uint32_t, uint32_t, uint32_t, double, int32_t);
-uint32_t otputArray(uint32_t, uint32_t, uint32_t, double, int32_t, uint32_t*&);
+int32_t countAmstrongNumber(uint32_t, uint32_t, uint32_t, double, int32_t, uint32_t);
+void remakeArray(uint32_t&, uint32_t, uint32_t, uint32_t, double, int32_t);
+uint32_t outputArray(uint32_t, uint32_t, uint32_t, double, int32_t, uint32_t*&);
 
 int main()
 {
-    uint32_t figure = 0, singleNumber = 0, num = 0;
+    uint32_t figure = 0, oneDigit = 0, num = 0, size = 10;
     double degree = 10;
     int32_t count = 0;
-    uint32_t* arrayForOutput = new uint32_t[countAmstrongNumber(num, singleNumber, figure, degree, count)];
     std::cout << "Enter number = ";
     std::cin >> num;
+    uint32_t sizeForArray = countAmstrongNumber(num, oneDigit, figure, degree, count, size);
+    uint32_t* arrayForOutput = new uint32_t[sizeForArray];
     if (num < 1)
     {
         std::cout << "Number must be a naturale digit\n";
     }
-    otputArray(num, singleNumber, figure, degree, count, arrayForOutput);
-    for (size_t i = 0; i < countAmstrongNumber(num, singleNumber, figure, degree, count); ++i)
+    outputArray(num, oneDigit, figure, degree, count, arrayForOutput);
+    for (size_t i = 0; i < sizeForArray; ++i)
     {
         std::cout << arrayForOutput[i] << "  It's Amstrong number\n";
     }
-    delete[]arrayForOutput;
+    delete[] arrayForOutput;
     return 0;
 }
 
-int32_t countAmstrongNumber(uint32_t num, uint32_t singleNumber, uint32_t figure, double degree, int32_t count)
+int32_t countAmstrongNumber(uint32_t num, uint32_t oneDigit, uint32_t figure, double degree, int32_t count, uint32_t size)
 {
-    uint32_t copy1 = 0, copy2 = 0;
-    uint32_t sum = 0, size = 10;
-    remakeArray:
+    uint32_t copy1 = 0, copy2 = 0, sum = 0;
     count = 0;
-    uint32_t* arr = new uint32_t[size];
     for (size_t i = 1; i < num; ++i)
     {
-        copy1 = copy2 = i;
+        copy1 = i;
+        copy2 = i;
         while (copy1)
         {
             copy1 /= 10;
             ++figure;
         }
-        singleNumber = pow(degree, figure - 1);
+        oneDigit = pow(degree, figure - 1);
         while (copy2)
         {
-            sum += pow((double)(copy2 / singleNumber), figure);
-            copy2 %= singleNumber;
-            singleNumber /= 10;
+            sum += pow((double)(copy2 / oneDigit), figure);
+            copy2 %= oneDigit;
+            oneDigit /= 10;
         }
         if (sum == i)
         {
             if (count == size)
             {
-                delete[]arr;
-                size += sqrt(size);
-                goto remakeArray;
+                remakeArray(size, num, oneDigit, figure, degree, count);
             }
-            arr[count++] = i;
+            count++;
         }
         figure = 0;
         sum = 0;
     }
-    delete[]arr;
-    return size;
+    return count;
 }
 
-uint32_t otputArray(uint32_t num, uint32_t singleNumber, uint32_t figure, double degree, int32_t count, uint32_t*& arrayForOutput)
+void remakeArray(uint32_t& size, uint32_t num, uint32_t oneDigit, uint32_t figure, double degree, int32_t count)
 {
-    uint32_t copy1 = 0, copy2 = 0;
-    uint32_t sum = 0;
-    count = -1;
+    size += sqrt(size);
+    countAmstrongNumber(num, oneDigit, figure, degree, count, size);
+}
+
+uint32_t outputArray(uint32_t num, uint32_t oneDigit, uint32_t figure, double degree, int32_t count, uint32_t*& arrayForOutput)
+{
+    uint32_t copy1 = 0, copy2 = 0, sum = 0;
+    count = 0;
     for (size_t i = 1; i < num; ++i)
     {
-        copy1 = copy2 = i;
+        copy1 = i;
+        copy2 = i;
         while (copy1)
         {
             copy1 /= 10;
             ++figure;
         }
-        singleNumber = pow(degree, figure - 1);
+        oneDigit = pow(degree, figure - 1);
         while (copy2)
         {
-            sum += pow((double)(copy2 / singleNumber), figure);
-            copy2 %= singleNumber;
-            singleNumber /= 10;
+            sum += pow((double)(copy2 / oneDigit), figure);
+            copy2 %= oneDigit;
+            oneDigit /= 10;
         }
         if (sum == i)
         {
-                arrayForOutput[++count] = i;
+                arrayForOutput[count++] = i;
         }
         figure = 0;
         sum = 0;
     }
     return *arrayForOutput;
 }
+
